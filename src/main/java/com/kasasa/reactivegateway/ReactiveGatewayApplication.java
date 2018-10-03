2b220cdcef388d4e4b0d3d797ab6b126521dba88
Camilo Pereira <c.pereira.leon@gmail.com>
2018-10-03 18:13:22 -0400

    Gateway naming and project structure added

--- src/main/java/com/kasasa/reactivegateway/ReactiveGatewayApplication.java
+++ src/main/java/com/kasasa/reactivegateway/ReactiveGatewayApplication.java
@@ -1,16 +1,25 @@
 package com.kasasa.reactivegateway;
 
+import com.kasasa.reactivegateway.handler.GatewayHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 public class ReactiveGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveGatewayApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveGatewayApplication.class, args);
+    }
 
-		GreetingWebClient gwc = new GreetingWebClient();
-		System.out.println(gwc.getResult());
+    @Bean
+    public RouterFunction<ServerResponse> route(GatewayHandler gatewayHandler) {
 
-	}
+        return RouterFunctions
+                .route(RequestPredicates.all(), gatewayHandler::handle);
+    }
 }
